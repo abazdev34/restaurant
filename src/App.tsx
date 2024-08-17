308661b5b752a07d24de5e96e2ffc923ea9ee808
abazdev34 <abaztoktorbaev89@gmail.com>
2024-08-17 10:34:00 +0300

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/header/index';
+import Home from './pages/home'
 // import Footer from './components/Footer';
-// import Home from './components/Home';
+
 // import Menu from './components/Menu';
 // import About from './components/About';
 // import Contact from './components/Contact';
@@ -13,13 +14,11 @@ const App: React.FC = () => {
       <Header />
       <main>
         <Routes>
-          {/* <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/" element={<Home/>}/>
+          
         </Routes>
       </main>
-      {/* <Footer /> */}
+     
     </div>
   );
 };
